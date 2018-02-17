179642138ca32818530b86f30e1732f2c549c619
Alwyn Tan <alwyn.tan@data.gov.sg>
2018-02-17 10:31:02 +0800

    Upgrade to Nuxt 1.3.0
    * Copy `nuxt-build` into `nuxt-es5`
    * `npm run build` - reference our nuxt-build, drop require for babel-register since no longer needed

--- template/scripts/build-nuxt-es5.js
+++ template/scripts/build-nuxt-es5.js
@@ -10,3 +10,6 @@ shell.rm('-rf', 'nuxt-es5/core')
 shell.exec('npx babel-cli -D -d nuxt-es5/builder node_modules/nuxt/lib/builder')
 shell.exec('npx babel-cli -D -d nuxt-es5/common node_modules/nuxt/lib/common')
 shell.exec('npx babel-cli -D -d nuxt-es5/core node_modules/nuxt/lib/core')
+
+shell.mkdir('nuxt-es5/bin')
+shell.cp('node_modules/nuxt/bin/nuxt-build', 'nuxt-es5/bin/nuxt-build')
